f0700862448698a619bab9be16bf8c84d1892af6
Dounya Bourhani <82082412+dounya-bourhani@users.noreply.github.com>
2024-03-13 16:37:51 +0100

    début page Accueil

--- front/Accueil.py
+++ front/Accueil.py
@@ -13,7 +13,22 @@ def main():
         layout="wide",
     )
 
-    st.title("Application voice-to-text Notebook Assistant")
+    st.title("Bienvenue sur Code Generation Assistant")
+
+    st.write("""L'application Code Generation est un outil innovant conçu pour aider les développeurs à générer du code rapidement et efficacement en utilisant un assistant virtuel.""")
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/typing.jpg", width=500)
+        st.write("""Que ce soit par le biais de requêtes textuelles écrites à la main...""")
+    
+    with col2:
+        st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/reco_vocale.jpg", width=500)
+        st.write("""ou de requêtes détectées par reconnaissance vocale...""")
+                 
+    st.write("""les utilisateurs peuvent interagir avec l'assistant pour 
+             exprimer leurs besoins en matière de code.""")
 
 if __name__ == "__main__":
     main()
