e154d88e38d83b9d2aea45f1c5d2c55728a2676a
joepelham <104430977+joepelham@users.noreply.github.com>
2024-03-14 11:26:28 +0100

    Update 2_🔗_Connexion_avec_Notebook.py

--- app/pages/2_🔗_Connexion_avec_Notebook.py
+++ app/pages/2_🔗_Connexion_avec_Notebook.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 
-
 ##################################### Functions ##########################################
 def file_path_uploader(label, file_types=".ipynb"):
     """
@@ -12,12 +11,28 @@ def file_path_uploader(label, file_types=".ipynb"):
         return file.name, file
     return None, None
 
+def submit_token():
+    if "token" not in st.session_state:
+            st.session_state.token = ""
+    if st.session_state.token_input:
+        st.session_state.token = st.session_state.token_input
+        st.session_state.token_input = ""
 ##################################### PAGES ##########################################
+st.set_page_config(
+        page_title="Speech-to-text",
+        page_icon="🔗",
+        layout="wide",
+    )
+
+
+st.sidebar.text_input("Insérer un token Hugging Face 🤗 :", key="token_input", on_change=submit_token, type = 'password')
+st.sidebar.button("Valider", on_click=submit_token)
+if "token" in st.session_state: 
+    st.sidebar.write("✅ Token activé")
 
 def file_con():
     # Utilisation du composant personnalisé
     file_name, file_content = file_path_uploader("Sélectionnez un notebook (.ipynb) :")
-    
     if file_name is None:
         st.write("🔴 Aucun notebook connecté")
     else:
@@ -29,13 +44,17 @@ def file_con():
         st.session_state['path'] = path
         st.session_state['file'] = file_name
 
-##################################### PAGES ##########################################
-st.set_page_config(
-        page_title="Speech-to-text",
-        page_icon="🔗",
-        layout="wide",
-    )
 
 st.title("Connexion avec Notebook")
 
-file_con()
+# Check if the file name is stored in the session state
+if 'file' in st.session_state:
+    st.write(f"✅ Connexion établie avec notebook '{st.session_state['file']}'.") 
+    path = os.path.abspath(st.session_state['file'])
+    st.write("Chemin : ", path)
+    if st.button("Connecter à un autre notebook"):
+        st.session_state.pop('file', None)
+        st.session_state.pop('path', None)
+        file_con()
+else:
+    file_con()
