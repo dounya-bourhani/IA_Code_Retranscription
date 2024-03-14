b816492dc682193a2c89ade0e0d381d89ddb2d6e
joepelham <104430977+joepelham@users.noreply.github.com>
2024-03-14 09:28:10 +0100

    Update Accueil.py

--- app/Accueil.py
+++ app/Accueil.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 
+
 def main():
     base = "dark"
     primaryColor = "#0079F7"
@@ -8,27 +9,40 @@ def main():
     font = "monospace"
 
     st.set_page_config(
-        page_title="Voice-to-text",
+        page_title="Speech-to-text",
         page_icon="🎙️",
         layout="wide",
     )
 
-    st.title("Bienvenue sur Code Generation Assistant")
+    st.title("Speech-to-text Notebook Assistant")
+
+    st.header("Bienvenue sur Code Generation Assistant")
+
+    st.sidebar.success("Sélectionner une page")
 
     st.write("""L'application Code Generation est un outil innovant conçu pour aider les développeurs à générer du code rapidement et efficacement en utilisant un assistant virtuel.""")
     
     col1, col2 = st.columns(2)
 
-    with col1:
-        st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/typing.jpg", width=500)
-        st.write("""Que ce soit par le biais de requêtes textuelles écrites à la main...""")
+    #with col1:
+    #    st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/typing.jpg", width=500)
+    #    st.write("""Que ce soit par le biais de requêtes textuelles écrites à la main...""")
     
-    with col2:
-        st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/reco_vocale.jpg", width=500)
-        st.write("""ou de requêtes détectées par reconnaissance vocale...""")
+    #with col2:
+    #    st.image("C:/Users/dbourhani/Documents/PROJET_IA/IA_Code_Retranscription/front/icons/reco_vocale.jpg", width=500)
+    #    st.write("""ou de requêtes détectées par reconnaissance vocale...""")
                  
     st.write("""les utilisateurs peuvent interagir avec l'assistant pour 
              exprimer leurs besoins en matière de code.""")
 
+
+
+    # Define values 
+    if 'path' not in st.session_state:
+            st.session_state['path'] = ""
+    if 'file' not in st.session_state:
+            st.session_state['file'] = ""
+
+    
 if __name__ == "__main__":
     main()
